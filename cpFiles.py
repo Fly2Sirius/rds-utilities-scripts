a5555f1aaeb683d88bf83105a6579053cca3b3ae
breakaway9 <breakaway9@gmail.com>
2021-09-28 11:12:09 -0600

    Adding script to get GoPro files copied locally and clean up

---
+++ cpFiles.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import shutil
+import os
+
+srcDir = '/Volumes/Untitled/DCIM/100GOPRO/'
+dstDir = '/Users/krisdavey/Movies/GoPro/'
+
+if(os.path.isdir(srcDir)):
+
+    for file in os.listdir(srcDir):
+        if file.endswith(".MP4"):
+            print(f'Copying {file} to {dstDir}')
+            shutil.move(os.path.join(srcDir, file),os.path.join(dstDir, file))
+
+    for file in os.listdir(srcDir):
+        print("Removing " + srcDir + file)
+        os.remove(srcDir + file)
+else : 
+     print("The drive doesn't seem to be inserted")
