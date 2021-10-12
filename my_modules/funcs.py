f9b60aa2cce1d56edd8aa28643704010f5f0a2f5
breakaway9 <breakaway9@gmail.com>
2021-10-12 17:16:33 -0600

    Reorg of files, adding funcs.py

---
+++ my_modules/funcs.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import os
+from configparser import ConfigParser
+import mysql.connector
+from mysql.connector import Error
+
+
+def get_mysql_credentials():
+    home_dir = Path.home()
+    conf = '.my.cnf'
+    conf = os.path.join(home_dir,conf)
+    config = ConfigParser()
+    config.read(conf)
+    mysql_user = config.get('client', 'user')
+    mysql_password = config.get('client', 'password')
+    return mysql_user,mysql_password
+
+def create_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+        sys.exit()
+
+    return connection
+
+def send_slack_notification(users):
+    s = requests.Session()
+    s.headers.update({"Authorization": "GenieKey 9f746c3e-659b-4fca-aa81-6d0fa8022378", "Content-Type": "application/json"})
+    url="https://api.opsgenie.com/v2/alerts"
+    data = {
+        "message": "Unknown MySQL Users Found",
+    "description": "Unknown Users : "+str(users),
+    "details": {
+        "team": "apiStatus",
+        "test": "true"
+        }
+    }       
+    #x = s.post(url, data=json.dumps(data))
+    print(json.dumps(data))
