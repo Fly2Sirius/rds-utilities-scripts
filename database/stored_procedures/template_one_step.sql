f2feb9ec1e398619a644a4af9d33740a1bbdf4c5
breakaway9 <breakaway9@gmail.com>
2021-11-17 10:57:31 -0700

    Adding templates

---
+++ database/stored_procedures/template_one_step.sql
@@ -0,0 +1,53 @@
+DROP PROCEDURE `datateam`.`backfill_2021116_update_bv_sourceId_from_temp_table`;
+
+DELIMITER ;;
+CREATE PROCEDURE `datateam`.`backfill_2021116_update_bv_sourceId_from_temp_table`(inStart int, inIncrement int)
+proc_Exit:BEGIN
+DECLARE loop_id int DEFAULT 1;
+DECLARE total_loops int;
+DECLARE loop_id_start int DEFAULT 1;
+DECLARE loop_id_end int DEFAULT 1;
+DECLARE max_temp_id int DEFAULT 1;
+DECLARE job_steps int DEFAULT 1;
+DECLARE last_loop_time int DEFAULT 1;
+DECLARE total_rows_to_update int DEFAULT 1;
+SET job_steps = 1;
+
+select max(id) into max_temp_id from optimus._temp_borrowerValue_sourceid;
+call `datateam`.`job_log_start`("stored proc name",job_steps,1,inIncrement,inStart,"Starting Job",@_log_id);
+SET total_rows_to_update = ((max_temp_id) - inStart);
+SET total_loops = ceiling(total_rows_to_update/inIncrement);
+IF inStart > 1 THEN
+	SET loop_id_start = inStart;
+ELSE 
+  	SET loop_id_start = 1;
+END IF;
+-- select total_loops;
+SET loop_id_end = (loop_id_start - 1) + inIncrement;
+select max_temp_id,inStart,total_rows_to_update,loop_id_end,total_loops;
+SET loop_id = 1;
+call `datateam`.`job_log_steps_start`(@_log_id,"The First Step",max_temp_id,inIncrement,inStart,max_temp_id,"Starting First Step",@_log_step_id);
+	UPDATE_LOOP:WHILE loop_id <= total_loops DO
+	   	select CURRENT_TIMESTAMP into @start_loop; 
+	   	IF loop_id_end > max_temp_id THEN
+	   		SET loop_id_end = max_temp_id;
+		END IF;
+	   	SET @sql = CONCAT('Query to loop through');
+		-- select @sql;
+	    PREPARE s1 FROM @sql;
+		EXECUTE s1;
+	        
+	    select CONCAT('Loop :',loop_id,' of ',total_loops) as LoopCounter;
+	    SET loop_id_start = loop_id_end;
+	    SET loop_id_end = loop_id_end + inIncrement; 
+	    SET loop_id = loop_id+1;   
+	    SELECT TIME_TO_SEC(TIMEDIFF(CURRENT_TIMESTAMP, @start_loop)) into last_loop_time;
+	    call `datateam`.`job_log_steps_update`("loops_complete",loop_id);
+		call `datateam`.`job_log_steps_update`("last_loop_time",last_loop_time);
+    END WHILE UPDATE_LOOP;
+call `datateam`.`job_log_steps_update`("end",CURRENT_TIMESTAMP);
+
+call `datateam`.`job_log_update`("status","Complete");
+call `datateam`.`job_log_update`("end",CURRENT_TIMESTAMP);
+END;;
+DELIMITER ;
---
+++ database/stored_procedures/template_three_step.sql
@@ -0,0 +1,113 @@
+DROP PROCEDURE IF EXISTS `datateam`.`template`;
+
+DELIMITER ;;
+CREATE PROCEDURE `datateam`.`template`(instart int, inIncrement int)
+proc_Exit:BEGIN
+DECLARE loop_id int DEFAULT 1;
+DECLARE total_loops int;
+DECLARE loop_id_start int DEFAULT 1;
+DECLARE loop_id_end int DEFAULT 1;
+DECLARE max_bv_id int DEFAULT 1;
+DECLARE job_steps int DEFAULT 1;
+DECLARE last_loop_time int DEFAULT 1;
+DECLARE total_rows_to_update int DEFAULT 1;
+SET job_steps = 3;
+
+
+-- Query to get the upper value to loop up to
+-- select max(id) into max_bv_id from optimus.borrowerValues;
+call `datateam`.`job_log_start`("stored proc name",job_steps,1,inIncrement,inStart,"Starting Job",@_log_id);
+
+SET total_rows_to_update = ((max_bv_id) - inStart);
+SET total_loops = ceiling(total_rows_to_update/inIncrement);
+
+-- Step 1
+
+call `datateam`.`job_log_update`("status","Running Step 1");
+call `datateam`.`job_log_steps_start`(@_log_id,"The First Step",max_bv_id,inIncrement,inStart,max_bv_id,"Starting First Step",@_log_step_id);
+
+IF inStart > 1 THEN
+	SET loop_id_start = inStart;
+ELSE 
+  	SET loop_id_start = 1;
+END IF;
+SET loop_id_end = (loop_id_start - 1) + inIncrement;
+SET loop_id = 1;
+
+INSERT_LOOP_1:WHILE loop_id <= total_loops DO
+	select CURRENT_TIMESTAMP into @start_loop;
+    SET @sql = CONCAT('1st Query to loop through');
+	PREPARE s1 FROM @sql;
+	EXECUTE s1;
+	select CONCAT('Loop :',loop_id,' of ',total_loops) as LoopCounter;
+    SET loop_id_start = loop_id_end;
+    SET loop_id_end = loop_id_end + inIncrement; 
+    SET loop_id = loop_id+1;
+	-- SELECT TIME_TO_SEC(TIMEDIFF(CURRENT_TIMESTAMP, @start_loop)) into last_loop_time;
+	call `datateam`.`job_log_steps_update`("loops_complete",loop_id);
+	call `datateam`.`job_log_steps_update`("last_loop_time",last_loop_time);
+END WHILE INSERT_LOOP_1;
+call `datateam`.`job_log_steps_update`("end",CURRENT_TIMESTAMP);
+
+-- Step 2
+
+call `datateam`.`job_log_update`("status","Running Step 2");
+call `datateam`.`job_log_steps_start`(@_log_id,"The Second Step",max_bv_id,inIncrement,inStart,max_bv_id,"Starting Second Step",@_log_step_id);
+
+IF inStart > 1 THEN
+	SET loop_id_start = inStart;
+ELSE 
+  	SET loop_id_start = 1;
+END IF;
+SET loop_id_end = (loop_id_start - 1) + inIncrement;
+SET loop_id = 1;
+
+INSERT_LOOP_2:WHILE loop_id <= total_loops DO
+        select CURRENT_TIMESTAMP into @start_loop;
+        SET @sql = CONCAT('2nd Query to loop through');
+		PREPARE s1 FROM @sql;
+		EXECUTE s1;
+		select CONCAT('Loop :',loop_id,' of ',total_loops) as LoopCounter;
+		SET loop_id_start = loop_id_end;
+		SET loop_id_end = loop_id_end + inIncrement; 
+		SET loop_id = loop_id+1;
+		SELECT TIME_TO_SEC(TIMEDIFF(CURRENT_TIMESTAMP, @start_loop)) into last_loop_time;
+		call `datateam`.`job_log_steps_update`("loops_complete",loop_id);
+		call `datateam`.`job_log_steps_update`("last_loop_time",last_loop_time);
+END WHILE INSERT_LOOP_2;
+call `datateam`.`job_log_steps_update`("end",CURRENT_TIMESTAMP);
+
+-- Step 3
+
+call `datateam`.`job_log_update`("status","Running Step 3");
+call `datateam`.`job_log_steps_start`(@_log_id,"The Third Step",max_bv_id,inIncrement,inStart,max_bv_id,"Starting Third Step",@_log_step_id);
+
+IF inStart > 1 THEN
+	SET loop_id_start = inStart;
+ELSE 
+  	SET loop_id_start = 1;
+END IF;
+SET loop_id_end = (loop_id_start - 1) + inIncrement;
+SET loop_id = 1;
+
+INSERT_LOOP_3:WHILE loop_id <= total_loops DO
+        select CURRENT_TIMESTAMP into @start_loop;
+        SET @sql = CONCAT('3rd Query to loop through.');
+        		PREPARE s1 FROM @sql;
+				EXECUTE s1;
+        
+        		select CONCAT('Loop :',loop_id,' of ',total_loops) as LoopCounter;
+        		SET loop_id_start = loop_id_end;
+        		SET loop_id_end = loop_id_end + inIncrement; 
+         		SET loop_id = loop_id+1;
+				SELECT TIME_TO_SEC(TIMEDIFF(CURRENT_TIMESTAMP, @start_loop)) into last_loop_time;
+		call `datateam`.`job_log_steps_update`("loops_complete",loop_id);
+		call `datateam`.`job_log_steps_update`("last_loop_time",last_loop_time);
+END WHILE INSERT_LOOP_3; 
+call `datateam`.`job_log_steps_update`("end",CURRENT_TIMESTAMP);
+
+call `datateam`.`job_log_update`("status","Complete");
+call `datateam`.`job_log_update`("end",CURRENT_TIMESTAMP);
+        
+END;;
+DELIMITER ;
