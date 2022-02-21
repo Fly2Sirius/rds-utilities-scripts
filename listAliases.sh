14ca8ac3a6f4b56a71e937f8dd6bab8de4ee9b58
sir_nebula <sir_nebula_84@protonmail.com>
2022-02-21 12:46:06 -0700

    Fixing list commands

--- listAliases.sh
+++ listAliases.sh
@@ -1,1 +1,2 @@
-gsed -n '/##/p' ~/.zshrc | gsed -re 's,\s+, ,g' | cut -d" " -f 2- | awk -F'[=#]' ' {print "\033[31m" $1 "\t  \033[34m"$(NF)}' | expand -t 15
+#gsed -n '/##/p' ~/.zshrc | gsed -re 's,\s+, ,g' | cut -d" " -f 2- | awk -F'[=#]' ' {print "\033[31m" $1 "\t  \033[34m"$(NF)}' | expand -t 15
+sed -n '/##/p' ~/.zshrc | sed -re 's,\s+, ,g' | cut -d" " -f 2- | awk -F'[=#]' ' {print "\033[31m" $1 "\t  \033[34m"$(NF)}' | expand -t 15
--- listDbs.sh
+++ listDbs.sh
@@ -1,2 +1,3 @@
 #cat $HOME/.zshrc | grep 'mysql --' | awk -F = '{ print $1 }' | awk '{ print $2 }'
-gsed -n '/mysql --/p' ~/.zshrc | gsed -re 's,\s+, ,g' | cut -d" " -f 2- | awk -F'[=#]' ' {print "\033[31m" $1 "\t  \033[34m"$(NF)}' | expand -t 18
+#gsed -n '/mysql --/p' ~/.zshrc | gsed -re 's,\s+, ,g' | cut -d" " -f 2- | awk -F'[=#]' ' {print "\033[31m" $1 "\t  \033[34m"$(NF)}' | expand -t 18
+sed -n '/mysql --/p' ~/.zshrc | sed -re 's,\s+, ,g' | cut -d" " -f 2- | awk -F'[=#]' ' {print "\033[31m" $1 "\t  \033[34m"$(NF)}' | expand -t 18
