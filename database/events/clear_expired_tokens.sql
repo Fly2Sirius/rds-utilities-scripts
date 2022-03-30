b5737cd0a3637bf2e05c7ea12e10ab4ac622e45c
Kris Davey <sir_nebula_84@protonmail.com>
2022-03-30 15:24:10 -0600

    Added event to clear expired oauth Tokens

---
+++ database/events/clear_expired_tokens.sql
@@ -0,0 +1,16 @@
+DROP EVENT IF EXISTS `datateam`.`clear_expired_tokens`;
+
+DELIMITER ;;
+
+CREATE EVENT `clear_expired_tokens` ON SCHEDULE EVERY 5 MINUTE STARTS '2022-03-30 14:25:00' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
+
+delete os,osat from oauth.oauth_sessions os
+join oauth.oauth_session_access_tokens osat on os.id = osat.session_id
+where osat.access_token_expires < unix_timestamp(current_timestamp);
+
+delete os from oauth_sessions os
+left join oauth_session_access_tokens osat on os.id = osat.session_id
+where osat.id is NULL;
+
+END;;
+DELIMITER ;
