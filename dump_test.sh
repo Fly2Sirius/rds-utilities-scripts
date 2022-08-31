c5450948e8dfcc2eba12d2e08d43fedd533e0872
Kris Davey <kris.davey@lendio.com>
2022-08-31 10:32:08 -0600

    Clenaing up sample and test scripts

--- dump_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-START="$(date +%s)"
-
-mysqldump --column-statistics=0 --set-gtid-purged=OFF -h homeportal-rc.cluster-civpyhkigzas.us-east-1.rds.amazonaws.com --databases wordpress | mysql -h hp-rc-clone-test2.cluster-civpyhkigzas.us-east-1.rds.amazonaws.com
-
-
-DURATION=$[ $(date +%s) - ${START} ]
-echo ${DURATION}
-
