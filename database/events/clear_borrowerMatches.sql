7fc914b573d3a330954aff89c619b712a7ade955
Kris Davey <kris.davey@lendio.com>
2022-10-26 16:26:41 -0600

    Updating notes and an SP.

--- database/events/clear_borrowerMatches.sql
+++ database/events/clear_borrowerMatches.sql
@@ -4,5 +4,8 @@ DELIMITER ;;
 
 CREATE EVENT `optimus`.`clear_borrowerMatches` ON SCHEDULE EVERY 1 DAY STARTS '2021-01-13 23:01:07' ON COMPLETION NOT PRESERVE ENABLE COMMENT 'Removes borrowerMatches records created 30 Days ago or greater' DO BEGIN
     CALL optimus.clear_borrower_matches();
+    CALL optimus.clear_borrowerMatchesClassifications(10);
+    CALL optimus.clear_borrowerMatchesLoanProductCategories(10);
+    CALL optimus.clear_borrowerMatchesLoanProducts(10);
 END;;
 DELIMITER ;
