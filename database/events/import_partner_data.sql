50f99a3f8ca5462ebdbfbfc5d04704ffe258e407
Kris Davey <kris.davey@lendio.com>
2023-05-17 11:17:39 -0600

    Updated fields yet again and added truncate command.

--- database/events/import_partner_data.sql
+++ database/events/import_partner_data.sql
@@ -2,11 +2,12 @@ DROP EVENT IF EXISTS `datateam`.`import_partner_data`;
 
 DELIMITER ;;
 
-CREATE EVENT `datateam`.`import_partner_data` ON SCHEDULE EVERY 5 MINUTE STARTS '2023-02-03 11:00:00' ON COMPLETION PRESERVE ENABLE COMMENT 'Imports partner affiliate ownership data.' DO BEGIN
-
-SET @timestamp = (select unix_timestamp(CURRENT_TIMESTAMP));
+CREATE EVENT `datateam`.`import_partner_data` ON SCHEDULE EVERY 5 MINUTE STARTS '2023-02-03 11:00:00' ON COMPLETION PRESERVE ENABLE COMMENT 'Imports partner data.' DO BEGIN
 
 call datateam.log('import_partner_data - Deals','start');
+
+truncate table `optimus`.`partnerDeals`;
+
 
 LOAD DATA FROM S3  'S3://lendio-snowflake-exports/deals/partner-affiliate-deals.csv'
     INTO TABLE `optimus`.`partnerDeals`
@@ -14,18 +15,16 @@ LOAD DATA FROM S3  'S3://lendio-snowflake-exports/deals/partner-affiliate-deals.
         OPTIONALLY ENCLOSED BY '"'
         LINES TERMINATED BY '\n'
         IGNORE 1 LINES
-        (@dummy,`dealId`,`borrowerId`,`dealType`,`productType`,`applicationStatus`,`term`,`dealClosedAt`,`fundedAmount`,`commissionAmount`,`payoutAmount`,`applicationStartedAt`,`applicationCompletedAt`,`dealSentAt`,`docPrepAt`,`offerReceived`,`offerReceivedCount`,`activityCount`,`marketingId`,`created`,`modified`,`parentDealId`,`offerAccepted`,`borrowerStage`)
+        (@dummy,`marketingId`,`ownershipStart`,`ownershipEnd`,`borrowerid`,`dealId`,`parentDealId`,`dealCreatedAt`,`dealType`,`productType`,`term`,`dealSentAt`,`docPrepAt`,`offerReceivedAt`,`offerRecievedCount`, `offerAcceptedAt`,`dealClosedAt`,`fundedAmount`,`commissionAmount`,`payoutAmount`,`salesRep`,`windowNumber`)
         
 call datateam.log(NULL,'end');
 
 
 
 
-
-
-SET @timestamp = (select unix_timestamp(CURRENT_TIMESTAMP));
-
 call datateam.log('import_partner_data - OwnershipWindows','start');
+
+truncate table `optimus`.`partnerOwnershipWindows`;
 
 LOAD DATA FROM S3  'S3://lendio-snowflake-exports/ownership/partner-affiliate-ownership.csv'
     INTO TABLE `optimus`.`partnerOwnershipWindows`
@@ -33,33 +32,10 @@ LOAD DATA FROM S3  'S3://lendio-snowflake-exports/ownership/partner-affiliate-ow
         OPTIONALLY ENCLOSED BY '"'
         LINES TERMINATED BY '\n'
         IGNORE 1 LINES
-        (`affiliateId`,`parentAffiliateId`,`borrowerId`,`marketingId`,`ownershipStart`,`ownershipEnd`,`borrowerName`,`doingBusinessAs`,`email`,`campaign`,`adGroup`,`medium`,`landingPage`,`firstContactedAt`,`applicationStartedAt`,`applicationCompletedAt`,`dealSentAt`,`docPrepAt`,`offerReceived`,`mineralGroup`,`borrowerMineralGroup`,`isQualified`,`dealClosedAt`,`lastLoginAt`,`borrowerCreatedAt`,`created`,`modified`)
+        (@dummy,`affiliateId`,`parentAffiliateId`,`marketingId`,`ownershipStart`,`ownershipEnd`,`borrowerid`,`borrowerName`,`doingBusinessAs`,`email`,`campaign`,`adGroup`,`medium`,`landingPage`,`mineralGroup`,`borrowerMineralGroup`,`isQualified`,`applicationStatus`,`borrowerStage`,`applicationStartedAt`,`applicationCompletedAt`,`firstAttemptedAt`,`firstContactedAt`,`dealCreatedAt`,`dealSentAt`,`docPrepAt`,`offerReceivedAt`,`offerAcceptedAt`,`dealClosedAt`,`salesRep`,`lastLoginAt`,`borrowerCreatedAt`)
         
 call datateam.log(NULL,'end');
-
-
-
-
-
-
-SET @timestamp = (select unix_timestamp(CURRENT_TIMESTAMP));
-
-call datateam.log('import_partner_data - partnerOpportunities','start');
-
-LOAD DATA FROM S3  'S3://lendio-snowflake-exports/opportunities/partner-affiliate-opportunities.csv'
-    INTO TABLE `optimus`.`partnerOpportunities`
-        FIELDS TERMINATED BY ','
-        OPTIONALLY ENCLOSED BY '"'
-        LINES TERMINATED BY '\n'
-        IGNORE 1 LINES
-        (`borrowerId`,`opportunityStartAt`,`opportunityEndAt`,`startReason`,`startMineralGroup`,`endMineralGroup`,`isQualifiedStart`,`isQualifiedEnd`,`firstContactedAt`,`applicationStartedAt`,`applicationCompletedAt`,`dealSentAt`,`docPrepAt`,`offerReceived`,`dealCreatedAt`,`dealClosedAt`,`activityCount`,`marketingId`,`created`,`modified`)
-        
-call datateam.log(NULL,'end');
-
 
 
 END;;
 DELIMITER ;
-
-
-
