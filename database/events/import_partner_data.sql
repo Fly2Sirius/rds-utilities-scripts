647a621bff1a0592dd1756fc374a54af9d09eeff
Kris Davey <kris.davey@lendio.com>
2023-07-25 13:38:44 -0600

    Added semi colons after load statements.

--- database/events/import_partner_data.sql
+++ database/events/import_partner_data.sql
@@ -2,7 +2,7 @@ DROP EVENT IF EXISTS `datateam`.`import_partner_data`;
 
 DELIMITER ;;
 
-CREATE EVENT `datateam`.`import_partner_data` ON SCHEDULE EVERY 5 MINUTE STARTS '2023-02-03 11:00:00' ON COMPLETION PRESERVE ENABLE COMMENT 'Imports partner data.' DO BEGIN
+CREATE EVENT `datateam`.`import_partner_data` ON SCHEDULE EVERY 1 HOUR STARTS '2023-07-024 11:00:00' ON COMPLETION PRESERVE ENABLE COMMENT 'Imports partner data.' DO BEGIN
 
 call datateam.log('import_partner_data - Deals','start');
 
@@ -15,7 +15,7 @@ LOAD DATA FROM S3  'S3://lendio-snowflake-exports/deals/partner-affiliate-deals.
         OPTIONALLY ENCLOSED BY '"'
         LINES TERMINATED BY '\n'
         IGNORE 1 LINES
-        (@dummy,`marketingId`,`ownershipStart`,`ownershipEnd`,`borrowerid`,`dealId`,`dealCreatedAt`,`dealStatus`,`dealStage`,`dealType`,`productType`,`term`,`dealSentAt`,`docPrepAt`,`offerReceivedAt`,`offerReceivedCount`, `offerAcceptedAt`,`dealClosedAt`,`fundedAmount`,`commissionAmount`,`payoutAmount`,`activityCount`,`salesRep`,`parentDealId`)
+        (@dummy,`marketingId`,`ownershipStart`,`ownershipEnd`,`borrowerid`,`dealId`,`dealCreatedAt`,`dealStatus`,`dealStage`,`dealType`,`productType`,`term`,`dealSentAt`,`docPrepAt`,`offerReceivedAt`,`offerReceivedCount`, `offerAcceptedAt`,`dealClosedAt`,`fundedAmount`,`commissionAmount`,`payoutAmount`,`activityCount`,`salesRep`,`parentDealId`);
         
 call datateam.log(NULL,'end');
 
@@ -32,7 +32,7 @@ LOAD DATA FROM S3  'S3://lendio-snowflake-exports/ownership/partner-affiliate-ow
         OPTIONALLY ENCLOSED BY '"'
         LINES TERMINATED BY '\n'
         IGNORE 1 LINES
-        (@dummy,`affiliateId`,`parentAffiliateId`,`marketingId`,`ownershipStart`,`ownershipEnd`,`borrowerid`,`borrowerName`,`doingBusinessAs`,`email`,`campaign`,`adGroup`,`medium`,`landingPage`,`mineralGroup`,`borrowerMineralGroup`,`isQualified`,`applicationStatus`,`borrowerStage`,`applicationStartedAt`,`applicationCompletedAt`,`firstAttemptedAt`,`firstContactedAt`,`dealCreatedAt`,`dealSentAt`,`docPrepAt`,`offerReceivedAt`,`offerAcceptedAt`,`dealClosedAt`,`salesRep`,`lastLoginAt`,`leadReceivedAt`)
+        (@dummy,`affiliateId`,`parentAffiliateId`,`marketingId`,`ownershipStart`,`ownershipEnd`,`borrowerid`,`borrowerName`,`doingBusinessAs`,`email`,`campaign`,`adGroup`,`medium`,`landingPage`,`mineralGroup`,`borrowerMineralGroup`,`isQualified`,`applicationStatus`,`borrowerStage`,`applicationStartedAt`,`applicationCompletedAt`,`firstAttemptedAt`,`firstContactedAt`,`dealCreatedAt`,`dealSentAt`,`docPrepAt`,`offerReceivedAt`,`offerAcceptedAt`,`dealClosedAt`,`salesRep`,`lastLoginAt`,`leadReceivedAt`);
         
 call datateam.log(NULL,'end');
 
@@ -47,7 +47,7 @@ LOAD DATA FROM S3  'S3://lendio-snowflake-exports/leads/partner-affiliate-leads.
     OPTIONALLY ENCLOSED BY '"'
     LINES TERMINATED BY '\n'
     IGNORE 1 LINES
-    (@dummy,`borrowerId`,`marketingId`,`ownershipMarketingId`,`ownershipStart`,`ownershipEnd`,`leadResult`,`leadReceivedAt`,`email`,`campaign`,`landingPage`,`capturePage`,`subId`,`adGroup`,`term`,`isQualified`)
+    (@dummy,`borrowerId`,`marketingId`,`ownershipMarketingId`,`ownershipStart`,`ownershipEnd`,`leadResult`,`leadReceivedAt`,`email`,`campaign`,`landingPage`,`capturePage`,`subId`,`adGroup`,`term`,`isQualified`);
 
 call datateam.log(NULL,'end');
 
